f4775b21101717e01424b34012594bf67c11ee0f
Manniec <manniec00@gmail.com>
2021-02-19 22:48:11 -0800

    Initial Commit

---
+++ Flix-iosUni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix-iosUni
+//
+//  Created by MannieC on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
